86afe558df4617ce8caa22c856a16f3d86ca7ded
khushi07-d <khushipanchal412@gmail.com>
2026-04-28 00:25:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# This is for Google verification file
-@app.route("/google30f660431520df61.html")
-def google_verification():
-    return send_from_directory(".", "google30f660431520df61.html")
 
 if __name__ == "__main__":
     app.run()
